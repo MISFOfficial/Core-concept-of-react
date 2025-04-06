b05a123db93a0bfa16b44bdd6249c89711b119d9
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-04-06 23:44:23 +0600

    new content added

--- src/Friends.jsx
+++ src/Friends.jsx
@@ -1,11 +1,18 @@
 import { use } from "react"
 
 export default function Friends({ callFriends }) {
-    const Friends = use(callFriends)
+    const friends = use(callFriends)
     return (
         <>
-            <div>
-                <h2>Total Friends: {Friends[0].name}</h2>
+            <div className="card">
+                <h2>Total Friends: {friends.length}</h2>
+                <div >
+                    {friends.map((friend) => (
+                        <p className="friend card"><span className="span">Name: </span>
+                            {friend.name}  <span className="span">Email: </span> {friend.email}
+                        </p>
+                    ))}
+                </div>
             </div>
         </>
     )
